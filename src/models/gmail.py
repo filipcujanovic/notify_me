18a89e73322648f602d970a59f2663a1ce660e52
Filip Cujanovic <filip.cujanovic@foxycart.com>
2020-09-15 20:22:52 +0200

    Put token.pickle dir location in .env

--- src/models/gmail.py
+++ src/models/gmail.py
@@ -16,8 +16,8 @@ class Gmail:
         # The file token.pickle stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
-        if os.path.exists('src/secrets/token.pickle'):
-            with open('src/secrets/token.pickle', 'rb') as token:
+        if os.path.exists(os.environ.get('TOKEN_PICKLE_DIR')):
+            with open(os.environ.get('TOKEN_PICKLE_DIR'), 'rb') as token:
                 creds = pickle.load(token)
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
@@ -27,7 +27,7 @@ class Gmail:
                 flow = InstalledAppFlow.from_client_secrets_file(os.path.abspath(os.environ.get('GMAIL_CREDENTIALS')), SCOPES)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with open('src/secrets/token.pickle', 'wb') as token:
+            with open(os.environ.get('TOKEN_PICKLE_DIR'), 'wb') as token:
                 pickle.dump(creds, token)
 
         return creds
